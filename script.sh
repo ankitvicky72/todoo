e506fd442e2bd14574e33d3b3ded72d934b114c7
ankitvicky72 <88142308+ankitvicky72@users.noreply.github.com>
2023-08-02 12:41:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,15 @@
+
 #!/bin/bash
-pip install django==3.2
-python manage.py migrate
-pip install -r /var/www/html/your-app-name/requirements.txt
+
+# Navigate to your application directory
+cd /var/www/html/your-app-name
+
+# Install Python and required packages
+yum install -y python3 python3-pip
+pip3 install -r requirements.txt
+
+# Run migrations
+python3 manage.py migrate
+
+# If you're using Gunicorn as the application server, start it
+# gunicorn -w 4 your_app_name.wsgi:application
